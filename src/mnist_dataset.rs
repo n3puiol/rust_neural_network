74a77f92761c8f42dc853434c15664618020d9f3
louis <louis.vanstappen@gmail.com>
2024-02-05 19:24:29 +0100

    implemented parsing of mnist

---
+++ src/mnist_dataset.rs
@@ -0,0 +1,25 @@
+use std::{error::Error};
+
+pub struct MnistRecord {
+    pub label: u8,
+    pub pixels: Vec<f64>,
+}
+
+pub fn load_mnist_dataset(data_path: &str) -> Result<Vec<MnistRecord>, Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_reader(std::fs::File::open(data_path)?);
+
+    let mut mnist_records = Vec::new();
+
+    for result in rdr.records().take(5) {
+        let record = result?;
+        let mut pixels = Vec::new();
+        for pixel in record.iter().skip(1) {
+            pixels.push(pixel.parse()?);
+        }
+        mnist_records.push(MnistRecord {
+            label: record[0].parse()?,
+            pixels,
+        });
+    }
+    Ok(mnist_records)
+}
