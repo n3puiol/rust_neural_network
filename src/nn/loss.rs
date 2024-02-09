e49cce9d58bd9aa20581244effee668b1a5e784c
louis <louis.vanstappen@gmail.com>
2024-02-09 03:27:05 +0100

    implemented mse

--- src/nn/loss.rs
+++ src/nn/loss.rs
@@ -1,27 +1,51 @@
 use crate::nn::utils::Vector;
 
 pub(crate) trait Loss {
-    fn loss(&self, outputs: &Vector, targets: &Vector) -> f64;
-
+    fn single_loss(&self, output: f64, target: f64) -> f64;
+    fn total_loss(&self, outputs: &Vector, targets: &Vector) -> f64;
     fn gradient(&self, outputs: &Vector, targets: &Vector) -> Vector;
 }
 
-pub struct SparseCategoricalCrossentropyLoss;
+pub struct MeanSquaredErrorLoss;
 
-impl Loss for SparseCategoricalCrossentropyLoss {
-    fn loss(&self, outputs: &Vector, targets: &Vector) -> f64 {
+impl Loss for MeanSquaredErrorLoss {
+    fn single_loss(&self, output: f64, target: f64) -> f64 {
+        (output - target).powi(2)
+    }
+
+    fn total_loss(&self, outputs: &Vector, targets: &Vector) -> f64 {
         let mut loss = 0.0;
         for (output, target) in outputs.iter().zip(targets.iter()) {
-            loss += target * output.log(0.0);
+            loss += self.single_loss(*output, *target);
         }
-        -loss
+        loss
     }
 
     fn gradient(&self, outputs: &Vector, targets: &Vector) -> Vector {
         let mut gradient = Vec::new();
         for (output, target) in outputs.iter().zip(targets.iter()) {
-            gradient.push(-target / output);
+            gradient.push(2.0 * (output - target));
         }
         Vector::from(gradient)
     }
 }
+
+// pub struct SparseCategoricalCrossentropyLoss;
+//
+// impl Loss for SparseCategoricalCrossentropyLoss {
+//     fn total_loss(&self, outputs: &Vector, targets: &Vector) -> f64 {
+//         let mut loss = 0.0;
+//         for (output, target) in outputs.iter().zip(targets.iter()) {
+//             loss += -target * output.ln();
+//         }
+//         loss
+//     }
+//
+//     fn gradient(&self, outputs: &Vector, targets: &Vector) -> Vector {
+//         let mut gradient = Vec::new();
+//         for (output, target) in outputs.iter().zip(targets.iter()) {
+//             gradient.push(-target / output);
+//         }
+//         Vector::from(gradient)
+//     }
+// }
