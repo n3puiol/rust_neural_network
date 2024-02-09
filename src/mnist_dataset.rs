49fe0b4a36c02354134adb06d9e28b9ddab7bd62
louis <louis.vanstappen@gmail.com>
2024-02-09 03:27:25 +0100

    added take to params

--- src/mnist_dataset.rs
+++ src/mnist_dataset.rs
@@ -5,12 +5,12 @@ pub struct MnistRecord {
     pub pixels: Vec<f64>,
 }
 
-pub fn load_mnist_dataset(data_path: &str) -> Result<Vec<MnistRecord>, Box<dyn Error>> {
+pub fn load_mnist_dataset(data_path: &str, nb_records: usize) -> Result<Vec<MnistRecord>, Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(std::fs::File::open(data_path)?);
 
     let mut mnist_records = Vec::new();
 
-    for result in rdr.records().take(5) {
+    for result in rdr.records().take(nb_records) {
         let record = result?;
         let mut pixels = Vec::new();
         for pixel in record.iter().skip(1) {
